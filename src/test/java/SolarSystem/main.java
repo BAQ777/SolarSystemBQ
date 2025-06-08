45583702fb75b0325cc816ff2d3fff8212eb7e63
BAQ777 <baquiroz777@gmail.com>
2025-06-08 11:13:07 -0400

    Revert "added feature sun"

--- src/test/java/SolarSystem/main.java
+++ src/test/java/SolarSystem/main.java
@@ -7,9 +7,5 @@ public class main {
         solar.sun="1";
         solar.planets="2";
         solar.stars="3";
-        
-        featureSun Xros=new featureSun();
-        Xros.color="pink";
-        Xros.temp="3400k";
     }
 }
